4ae45aad96a1da2a81f5995df28f60e36c0176a5
Benjamin Glouton <benjie@benjamins-mbp.myfiosgateway.com>
2020-08-07 23:45:31 -0400

    ‘whateverMsg’

--- client/src/components/views/Request/Request.js
+++ client/src/components/views/Request/Request.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { withRouter } from "react-router-dom";
+import MyForm from "./myform.js";
 
 function Request ( props ) {
     return ( 
         <Fragment>
         <div className="app">
         <h1>Request a Quote</h1>
+        <MyForm/>
         {/* Look into implementing a Wizard form https://redux-form.com/8.2.2/examples/wizard/ */}
         </div>
         </Fragment>
---
+++ client/src/components/views/Request/myform.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+export default class MyForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+        status: ""
+        };
+    }
+
+    render() {
+        const { status } = this.state;
+        return (
+        <form
+            onSubmit={this.submitForm}
+            action="https://formspree.io/xyynolko"
+            method="POST"
+        >
+            <div className="form-group">
+						<div className="row">
+							<div className="col-12 col-sm-12 col-md-6 mx-auto">
+								<input
+									type="text"
+									name="name"
+									className="form-control form-control-lg"
+									id="name"
+									placeholder="Full Name"
+								/>
+							</div>
+						</div>
+					</div>
+                    <div className="form-group hidden">
+						<div className="row">
+							<div className="col-12 col-sm-12 col-md-6 mx-auto">
+								<input
+									type="email"
+									name="_replyto"
+									className="form-control form-control-lg"
+									id="exampleFormControlInput1"
+									placeholder="Your email"
+								/>
+							</div>
+						</div>
+					</div>
+                    <div className="form-group hiddenLeft">
+						<div className="row">
+							<div className="col-12 col-sm-12 col-md-6 mx-auto">
+								<input
+									type="text"
+									name="subject"
+									className="form-control form-control-lg"
+									id="subject"
+									placeholder="Subject"
+								/>
+							</div>
+						</div>
+					</div>
+                    <div className="form-group hiddenRight">
+						<div className="row">
+							<div className="col-12 col-sm-12 col-md-6 mx-auto">
+								<textarea
+									name="message"
+									className="form-control form-control-lg"
+									id="exampleFormControlTextarea1"
+									rows="4"
+									placeholder="How can we assist you today?"
+								/>
+							</div>
+						</div>
+					</div>
+                    <div className="row text-md-right text-sm-center">
+						<div className="col-12 col-sm-12 col-md-6 mx-auto">
+                        {status === "SUCCESS" ? <p>Your Request has been Recieved!</p> : <button
+								type="submit"
+								className="btn btn-primary mb-2 hidden">
+								Submit
+							</button>}
+                            {status === "ERROR" && <p>Please Try Again, There was an error.</p>}
+						</div>
+					</div>
+        </form>
+        );
+    }
+
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            form.reset();
+            this.setState({ status: "SUCCESS" });
+        } else {
+            this.setState({ status: "ERROR" });
+        }
+        };
+        xhr.send(data);
+    }
+    }
